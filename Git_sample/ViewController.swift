67148b9692a83bb83f1463b8853632e7818d1067
佐川開人 <kaito.s.530@ruri.waseda.jp>
2018-05-06 20:54:16 +0900

    変更プラクティス

--- Git_sample/ViewController.swift
+++ Git_sample/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        let textLabel = makeTextLabel()
+    }
+    func makeTextLabel(){
         let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
         textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
         textLabel.backgroundColor = UIColor.redColor()
@@ -26,6 +30,7 @@ class ViewController: UIViewController {
         textLabel.layer.cornerRadius = 20.0
         self.view.addSubview(textLabel)
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
